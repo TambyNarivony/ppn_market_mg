f2509509dc6a019b1361c9bdd609897ffc3b4a63
Tamby Narivony <tambynarivony19@gmail.com>
2025-06-29 03:43:29 -0700

    resolution erreur

--- backend/config/database.php
+++ backend/config/database.php
@@ -52,19 +52,22 @@ return [
 // config/database.php
 'mysql' => [
     'driver' => 'mysql',
-    'host' => env('DB_HOST', 'gateway01.us-west-2.prod.aws.tidbcloud.com'),
-    'port' => env('DB_PORT', '4000'),
-    'database' => env('DB_DATABASE', 'test'),
-    'username' => env('DB_USERNAME', '3LKVQmGyDcMm13w.root'),
-    'password' => env('DB_PASSWORD', 't11637voAmP23Hvn'),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'unix_socket' => env('DB_SOCKET', ''),
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => '',
+    'strict' => true,
+    'engine' => null,
     'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA'),
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY_SERVER_CERT', true),
+        PDO::MYSQL_ATTR_SSL_CA => base_path(env('DB_SSL_CA')),
     ]) : [],
 ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
