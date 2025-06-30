5771a65b98f1d2d46366dacf29af50a69c9dc2c4
Tamby Narivony <tambynarivony19@gmail.com>
2025-06-29 23:28:16 -0700

    frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://ppn-market-mg-backend1.onrender.com/api';
+const API_BASE_URL = window.APP_CONFIG?.API_BASE_URL || 
+                    'https://ppn-market-mg-backend1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
